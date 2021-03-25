706606911c4a425a3022af21cd40f18982f8e94a
Satheesh Kumar <mail@satheesh.dev>
2021-03-25 16:56:56 +0530

    adds initial spring boilerplate code

---
+++ spring/src/test/java/com/hackerearth/main/BackendMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerearth.fullstack.backend.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
